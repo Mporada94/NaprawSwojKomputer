09f4b60a626d4ccef6a54e702a1821853625cd68
DESKTOP-A4EBB5L\mateusz <mporada94@outlook.com>
2018-03-23 20:00:11 +0100

    Utworzenie projektu

---
+++ NaprawSwojKomputer/NaprawSwojKomputer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NaprawSwojKomputer.Startup))]
+namespace NaprawSwojKomputer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
